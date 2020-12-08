b202074a05e2d98ac725d36d9c2c504578df9bf8
Victor Svanqvist <svvi19ty@mace2428-11.jth-stud2.hj.se>
2020-12-08 16:37:14 +0100

    COMMIT AGAIN

--- JU Quiz/StartViewController.swift
+++ JU Quiz/StartViewController.swift
@@ -9,13 +9,34 @@ import UIKit
 
 class StartViewController: UIViewController {
     
+    
     @IBOutlet weak var startButton: UIButton!
     var questions: [Question] = []
+    
+    
+    @IBOutlet weak var roundScroller: UIPickerView!
+    var pickerData: [Int] = [Int]()
+    
+    var numberOfQuestionss = 1
+    
+    @IBOutlet weak var difficultyScroller: UIPickerView!
+    var difficultyPickerData: [String] = [String]()
+    
+    var difficulty = "easy"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         startButton.isEnabled = false
         // Do any additional setup after loading the view.
-        downloadQuestions(ammount: 5)
+        self.roundScroller.delegate = self
+        self.roundScroller.dataSource = self
+        
+        self.difficultyScroller.delegate = self
+        self.difficultyScroller.dataSource = self
+        
+        difficultyPickerData = ["easy", "medium", "hard"]
+        pickerData = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
     }
 
     @IBAction func highScoreButton(_ sender: Any) {
@@ -24,8 +45,8 @@ class StartViewController: UIViewController {
     }
     
     
-    private func downloadQuestions(ammount: Int){
-        guard let url = URL(string:"https://opentdb.com/api.php?amount=\(ammount)&type=multiple") else{
+    private func downloadQuestions(ammount: Int, dif: String){
+        guard let url = URL(string:"https://opentdb.com/api.php?amount=\(ammount)&difficulty=\(dif)&type=multiple") else{
             return
         }
         let task = URLSession.shared.dataTask(with: url) { [weak self] (data, monkey, error) in
@@ -50,12 +71,49 @@ class StartViewController: UIViewController {
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if let questionViewController = segue.destination as? QuestionViewController{
-            //let question1 = Question(category: "Food", type: .multiple, difficulty: .easy, question: "How are you today?", correctAnswer: "Bad", incorrectAnswers: ["Good", "Medium", "Not bad"])
-            //let question2 = Question(category: "Food", type: .multiple, difficulty: .easy, question: "How are you today?", correctAnswer: "Bad", incorrectAnswers: ["Good", "Medium", "Not bad"])
-            //let questions = [question1, question2]
-            questionViewController.numberOfQuestions = questions.count
+            questionViewController.numberOfQuestions = numberOfQuestionss
             questionViewController.questions = questions
         }
     }
 
+}
+
+extension StartViewController: UIPickerViewDelegate, UIPickerViewDataSource{
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        if(pickerView == roundScroller){
+            return pickerData.count
+        }
+        if(pickerView == difficultyScroller){
+            return difficultyPickerData.count
+        }
+        return 0
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String?{
+        
+        print(pickerData[row])
+        if(pickerView == roundScroller){
+            return String(pickerData[row])
+        }
+        if(pickerView == difficultyScroller){
+            return difficultyPickerData[row]
+        }
+        return ""
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        print(pickerData[row])
+        if(pickerView == roundScroller){
+            numberOfQuestionss = pickerData[row]
+        }
+        if(pickerView == difficultyScroller){
+            difficulty = difficultyPickerData[row]
+        }
+        downloadQuestions(ammount: numberOfQuestionss, dif: difficulty)
+    }
+    
 }
