c6ddf13111df278c6fcccebfc009abb027af2ea8
Victor Svanqvist <svvi19ty@mace2428-11.jth-stud2.hj.se>
2020-12-08 16:27:42 +0100

    Klar med all...

---
+++ JU Quiz/StarterView.swift
@@ -0,0 +1,20 @@
+//
+//  StarterView.swift
+//  JU Quiz
+//
+//  Created by Victor Svanqvist on 2020-12-08.
+//
+
+import UIKit
+
+class StarterView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
